19eb6a42829916cbe9624f6ad218e3a535aca894
Andy Shtefan main <andyshtefan@gmail.com>
2023-07-12 09:27:57 +0300

    section added

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,15 @@ console.log(sum(2, 5));
 
 // !!! delete
 import gsapTest from "./module/TESTgsap.js";
-import swiperTest from "./module/TESTswiper.js";
-import sss from "./module/sss.js";
+// import swiperTest from "./module/TESTswiper.js";
+
+// import teamSlider from "./module/teamSlider.js";
+// import projectSlider from "./module/projectSlider.js";
 // !!! delete
 
-
-import teamSlider from "./module/teamSlider.js";
-import projectSlider from "./module/projectSlider.js";
+ 
 import scrollNburger from "./module/burgerNscroll.js";
+import featuresSwiper from "./module/featuresSwiper.js";
 // scrollNburger();
 
 
---
+++ src/js/module/featuresSwiper.js
@@ -0,0 +1,60 @@
+import Swiper from 'swiper/bundle';
+
+const swiperTest = new Swiper('.features__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 4,
+  //сколько слайдов проматываем
+  slidesPerGroup: 1,
+  slideToClickedSlide: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.features__pagination',
+    clickable: true,
+    type: "bullets",
+    // type: "fraction",
+    // type:"progressbar",
+    // type:"custom"
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    776: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1024: {
+      // slidesPerView: 4,
+      enabled: false,
+    },
+  },
+
+  // on: {
+  //   resize: function () {
+  //     swiper.changeDirection(getDirection());
+  //   },
+  // },
+
+});
+
+// function getDirection() {
+//   var windowWidth = window.innerWidth;
+//   var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+//   return direction;
+// }
+
+
+
+
+
+
+export default featuresSwiper;
